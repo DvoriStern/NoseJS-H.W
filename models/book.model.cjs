7c892190c77c51cbcc246cbe9ae4199e2f14339e
DvoriStern <dvori8847@gmail.com>
2024-04-02 17:36:09 +0300

    lsn10

--- models/book.model.cjs
+++ models/book.model.cjs
@@ -1,5 +1,8 @@
 //import { model, Schema } from 'mongoose';
+// const { default: Joi } = require('joi');
 const mongoose = require('mongoose');
+
+
 
 const bookSchema = new mongoose.Schema({
     name: {
@@ -16,16 +19,31 @@ const bookSchema = new mongoose.Schema({
         type:String,
         minlength:2,
     } ,
-    isLend: Boolean,
+    isLend: {type:Boolean,default:false},
     numPages:{type:Number,
         minlength:1,
     },
     publishDate:{
         type:Date,
         default:Date.now
-    }
+    },
+    authers:[String],
+    borrower: {
+        type: mongoose.Types.ObjectId, ref: 'borrower',
+        validate: {
+            validator(v) {
+                return this.isLend===true
+            },
+            required:true
+        }
+    },
 });
-
+// module.exports.bookValidator = {
+//     newSchema: Joi.object({
+//         name: Joi.string().min(2).required(),
+//         authers: Joi.min(1).required()
+//     }),
+// }
 
 // const Book = model('books', bookSchema);
 
